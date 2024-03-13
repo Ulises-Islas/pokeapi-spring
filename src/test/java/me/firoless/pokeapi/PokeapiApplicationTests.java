9ef0978622d0ba12191f326d1b797056db83bdcb
Ulises-Islas <emyises18012@gmail.com>
2024-03-12 18:57:33 -0600

    first commit

---
+++ src/test/java/me/firoless/pokeapi/PokeapiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.firoless.pokeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
